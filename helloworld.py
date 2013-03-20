f3b739d5c77c43226088788d0f06285caf2f173d
Bill Bushey <wbushey@gmail.com>
2013-03-20 14:50:06 -0500

    Added gdata library Successfully did a test of the gdata library Turned edemsignup.config into settings.py, and make it work

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,20 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
+
 import webapp2
+import gdata.docs.service
+from settings import gdocs_settings
 
 class MainPage(webapp2.RequestHandler):
   def get(self):
+      client = gdata.docs.service.DocsService()
+      client.ClientLogin(gdocs_settings['username'], gdocs_settings['password'])
+      documents_feed = client.GetDocumentListFeed()
+      retval = ''
+      for entry in documents_feed.entry:
+        retval += entry.title.text + "\n"
       self.response.headers['Content-Type'] = 'text/plain'
-      self.response.write('Hello, World!')
+      self.response.write(retval)
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
