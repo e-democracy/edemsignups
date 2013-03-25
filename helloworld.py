a8e394786fab81c2a6e300505592f26cdec6668e
Bill Bushey <wbushey@gmail.com>
2013-03-25 16:46:31 -0500

    Moved Docs and Spreadsheets Clients into properties Moved authentication token into property Created a recursive generator for getting spreadsheets from a folder Relying more on the Resource model

--- helloworld.py
+++ helloworld.py
@@ -9,54 +9,86 @@ import gdata.spreadsheet
 from settings import gdocs_settings
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
 
-      # This is a bit confusing, because the Spreadsheets Client does not 
-      # directly support searching for a folder. So, a Docs Client is first
-      # used to search for the folder we want to retreive spreadsheets from.
-      dclient = DocsClient()
-      query = DocsQuery(
-        title='Sign Up Spreadsheets (Data)',
-        title_exact='true',
-        show_collections='true')
+    def __init__(self, request, response):
+        # webapp2 uses initialize instead of __init__, cause it's special
+        self.initialize(request, response)
+        self.__token__ = None
+        self.__docsClient__ = None
+        self.__spreadsheetsClient__ = None
+    
+    @property
+    def docsClient(self):
+        if self.__docsClient__ is None:
+            self.__docsClient__ = DocsClient()
+        assert self.__docsClient__
+        return self.__docsClient__
 
-      # For now, being lazy and using username/password. The token returned by 
-      # one type of client can be used by other types. In fact, it has to, as
-      # future calls to ClientLogin will result in unusable tokens.
-      #
-      # Eventually, we should use Oauth.
-      token = dclient.ClientLogin(gdocs_settings['username'], 
+    @property
+    def spreadsheetsClient(self):
+        if self.__spreadsheetsClient__ is None:
+            self.__spreadsheetsClient__ = SpreadsheetsClient()
+        assert self.__spreadsheetsClient__
+        return self.__spreadsheetsClient__
+
+    @property
+    def token(self):
+        # For now, being lazy and using username/password. The token returned by 
+        # one type of client can be used by other types. In fact, it has to, as
+        # future calls to ClientLogin will result in unusable tokens.
+        #
+        # Eventually, we should use Oauth.
+        if self.__token__ is None:
+            self.__token__ = self.docsClient.ClientLogin(gdocs_settings['username'], 
                     gdocs_settings['password'], gdocs_settings['app_name'])
-      folder = dclient.GetResources(q=query).entry[0]
+        assert self.__token__
+        return self.__token__
 
-      # Now that we have the folder, its time to get the spreadsheets...
-      sclient = SpreadsheetsClient()
-      # Unfortunately, we can't just use the get_spreadsheets method, because 
-      # we need to provide the URI of the specific folder we want to get 
-      # spreadsheets from. A get_spreadsheets_from_folder function would be a
-      # nice addition.
-      spreadsheets = sclient.get_feed(uri=folder.content.src, 
-                    desired_class=SpreadsheetsFeed, auth_token=token)
+    def spreadsheets(self, folder):
+        '''Generates a list of Google Spreadsheets based on the Resource
+           instance provided, which is assumed to be a folder. If the provided
+           folder contains other folders, they will be recursively searched for
+           spreadsheets and other folders, breadth first.
+           
+           Returned spreadsheets will be Resource instances.'''
+        folders = []
+        contents = self.docsClient.GetResources(uri=folder.content.src,
+                    auth_token=self.token)
+        for entry in contents.entry:
+            if entry.GetResourceType() == 'folder':
+                folders.append(entry)
+            elif entry.GetResourceType() == 'spreadsheet':
+                yield entry
 
-      retval = 'Your Spreadsheets:\n'
-      #for entry in spreadsheets.entry:
-        #retval += "%s\n" % entry.category
-      for i, entry in enumerate(spreadsheets.entry):
-          if isinstance(spreadsheets, gdata.spreadsheet.SpreadsheetsCellsFeed):
-              print '%s %s\n' % (entry.title.text, entry.content.text)
-          elif isinstance(spreadsheets, gdata.spreadsheet.SpreadsheetsListFeed):
-              print '%s %s %s' % (i, entry.title.text, entry.content.text)
-              # Print this row's value for each column (the custom dictionary is
-              # built using the gsx: elements in the entry.)
-              print 'Contents:'
-              for key in entry.custom:  
-                  print '  %s: %s' % (key, entry.custom[key].text)
-              print '\n',
-          else:
-              print '%s %s\n' % (i, entry.title.text)
+        for folder in folders:
+            for spreadsheet in self.spreadsheets(folder):
+                yield spreadsheet
 
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.write(retval)
+
+    def get(self):
+        # This is a bit confusing, because the Spreadsheets Client does not 
+        # directly support searching for a folder. So, a Docs Client is first
+        # used to search for the folder we want to retreive spreadsheets from.
+        query = DocsQuery(
+            title='Sign Up Spreadsheets (Data)',
+            title_exact='true',
+            show_collections='true')
+
+        folder = self.docsClient.GetResources(q=query, auth_token=self.token).entry[0]
+
+        # Now that we have the folder, its time to get the spreadsheets...
+        # Unfortunately, we can't just use the get_spreadsheets method, because 
+        # we need to provide the URI of the specific folder we want to get 
+        # spreadsheets from, and we need to recurse on other folders. A 
+        # get_spreadsheets_from_folder function would be a nice addition.
+        
+        retval = 'Your Spreadsheets:\n'
+        for spreadsheet in self.spreadsheets(folder):
+            retval += '%s\n' % (spreadsheet.title.text)
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(retval)
+
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
