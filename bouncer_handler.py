3731d4829f74bd95ff2b016b7c36c82308f81402
Bill Bushey <wbushey@gmail.com>
2013-04-02 09:10:24 -0500

    Added Clients object property to email_bouncer Set all source to be utf-8

--- bouncer_handler.py
+++ bouncer_handler.py
@@ -1,16 +1,30 @@
+# coding=utf-8
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 
 from google.app.engine.ext.webapp.mail_handlers import BounceNotification,\
                                                     BounceNotificationHandler
-from gdata.docs.client import DocsClient, DocsQuery
-from gdata.spreadsheets.client import SpreadsheetsClient, ListQuery 
+from gdata.spreadsheets.client import ListQuery 
 from gdata.spreadsheets.data import SpreadsheetsFeed
 from settings import gdocs_settings
+from clients import Clients
 
 import logging
 
 class BounceHandler(BounceNotificationHandler):
+
+    def __init__():
+        super(BounceHandler, self).__init__()
+        self.__clients__ = None
+
+    @property
+    def clients(self):
+        if self.__clients__ is None:
+            self.__clients__ = Clients()
+
+        assert self.__clients__
+        return self.__clients__
+
 
     def receive(self, bounce_notification):
         logging.info('Received bounce form %s' % bounce_notification.notification_from)
--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 
