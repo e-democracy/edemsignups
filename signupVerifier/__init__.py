e25ac746aadd246d22c85d9a3e35a88e69f26382
Bill Bushey <wbushey@gmail.com>
2013-04-24 08:26:08 -0500

    Refactored signupVerifier to have processors, models, and io folders

--- signupVerifier/__init__.py
+++ signupVerifier/__init__.py
@@ -1,4 +1,12 @@
 # coding=utf-8
+
 import sys, os                                                                  
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),       
                 '../lib'))  
+
+# See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages  
+try:                                                                            
+    __import__('pkg_resources').declare_namespace(__name__)                     
+except ImportError:                                                             
+    from pkgutil import extend_path                                             
+    __path__ = extend_path(__path__, __name__)  
---
+++ signupVerifier/processors/__init__.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+from bounce_processor import BounceProcessor
+from initial_processor import InitialProcessor
+from optout_processor import OptOutProcessor
