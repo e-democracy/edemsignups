a0417b3e000952d87b03a7bd8a20a8cb4ea1fd8e
William Bushey <wbushey@gmail.com>
2013-05-01 14:52:54 -0500

    Added some logging to InitialSpreadsheetPage

--- controller.py
+++ controller.py
@@ -30,14 +30,23 @@ class SpreadsheetInitialPage(webapp2.RequestHandler):
         return self.__gclient__
 
     def get(self):
-        #   1.) Get list of all spreadsheets in folder (GClient)
+        # 0.) Setup output lists
+        batch_logs = []
+        def new_batch_log(meta_dict, spreadsheet_url):
+            return {'meta_dict' : meta_dict, 'spreadsheet_url': spreadsheet_url,
+                    'error': None, 'persons_success': [], 'persons_fail': []}
+
+        # 1.) Get list of all spreadsheets in folder 
         signups_folder = self.gclient.docsClient.GetResourceById(
                                                 settings['signups_folder_id')
         spreadsheets = self.gclient.spreadsheets(signups_folder)
-        #   2.) Discard from that list all spreadsheets already processed
+
+        # 2.) Discard from that list all spreadsheets already processed
         new_spreadsheets = self.gclient.filterOutOldSpreadsheets(spreadsheets)
-        #   3.) For all remaining spreadsheets, 
+
+        # 3.) Process the remaining spreadsheets
         for new_spreadsheet in new_spreadsheets:
+
             #  1.) Convert spreadsheets to batch_dict and person_dict (GClient)
             meta_list_feed = self.gclient.getMetaListFeed(new_spreadsheet)
             meta_dict = self.gclient.metaRowToDict(meta_list_feed)
@@ -52,90 +61,60 @@ class SpreadsheetInitialPage(webapp2.RequestHandler):
             # TODO add a step to validate the data of each person
             persons = []
             optout_tokens = dict()
-            if 'prev_batch' in meta_dict:
-                batch = addBatchChange(meta_dict, meta_dict['prev_batch'])
-                batchSpreadsheet = self.gclient.importBatchSpreadsheet(batch,
-                                        new_spreadsheet)
-                for person_dict in person_dicts:
-                    if 'person_id' in person_dict:
-                        person_dict['source_batch'] = batch.key()
-                        person = addPersonChange(person_dict, 
-                                    person_dict['person_id'])
-                    else:
-                        person = importPerson(person_dict, batch)
+            batch_log = new_batch_log(meta_dict, 
+                                        new_spreadsheet.FindHtmlLink())
+            if 'prevbatch' in meta_dict:
+                try:
+                    batch = addBatchChange(meta_dict, meta_dict['prevbatch'])
+                    batchSpreadsheet = self.gclient.importBatchSpreadsheet(
+                                                        batch,new_spreadsheet)
+                except Error as e:
+                    # Go no futher with processing this batch
+                    batch_log.error = e
+                    continue
 
-                    persons.append(person)
-                    optout_tokens[person.key()] = createOptOutToken(batch,
+                for person_dict in person_dicts:
+                    try:
+                        if 'personid' in person_dict:
+                            person_dict['source_batch'] = batch.key()
+                            person = addPersonChange(person_dict, 
+                                                    person_dict['personid'])
+                        else:
+                            person = importPerson(person_dict, batch)
+
+                        persons.append(person)
+                        optout_tokens[person.key()] = createOptOutToken(batch,
                                                                     person)
+                    except Error as e:
+                        new_batch_log.persons_fail.append((person_dict, e))
             else:
-                batch = importBatch(meta_dict)
-                batchSpreadsheet = self.gclient.importBatchSpreadsheet(batch,
-                                        new_spreadsheet)
+                try:
+                    batch = importBatch(meta_dict)
+                    batchSpreadsheet = self.gclient.importBatchSpreadsheet(
+                                                        batch, new_spreadsheet)
+                except Error as e:
+                    # Go no futher with processing this batch
+                    batch_log.error = e
+                    continue
+
                 for person_dict in person_dicts:
-                    person = importPerson(person_dict, batch)
-                    persons.append(person)
-                    optout_tokens[person.key()] = createOptOutToken(batch,
-                                                                    person)
+                    try:
+                        person = importPerson(person_dict, batch)
+                        persons.append(person)
+                        optout_tokens[person.key()] = createOptOutToken(batch,
+                                                                        person)
+                    except Error as e:
+                        new_batch_log.persons_fail.append((person_dict, e))
             # 4.) Generate and send Emails! 
-            sendVerificationEmails(batch)
-
-        # This is a bit confusing, because the Spreadsheets Client does not 
-        # directly support searching for a folder. So, a Docs Client is first
-        # used to search for the folder we want to retreive spreadsheets from.
-        query = DocsQuery(
-            title='Sign Up Spreadsheets (Data)',
-            title_exact='true',
-            show_collections='true')
-
-        folder = self.clients.docs.GetResources(q=query).entry[0]
-
-        retval = 'Your Spreadsheets:\n'
-        for spreadsheet in self.spreadsheets(folder):
-            # Another pain in the butt: as far as I can tell, there is no way 
-            # to convert a Resource into another object. The Spreadsheet class
-            # includes a GetSpreadsheetKey method that does the following. But
-            # since I can't just convert the Resource into a Spreadsheet, I
-            # have to get all goofy and reimplement GetSpreadsheetKey
-            spreadsheet_id = spreadsheet.GetId().split('/')[-1]
-            spreadsheet_id = spreadsheet_id.split('spreadsheet%3A')[-1]
-            retval += '%s\n' % spreadsheet.title.text
-            for worksheet in self.clients.spreadsheets.GetWorksheets(
-                                                        spreadsheet_id).entry:
-                retval += '\t%s\n' % worksheet.title.text
-                worksheet_id = worksheet.id.text.rsplit('/',1)[1]
-                if worksheet.title.text == 'Raw':
-                    rows = self.clients.spreadsheets.GetListFeed(
-                                        spreadsheet_id, worksheet_id).entry
-                    for row in rows:
-                        template_values = {
-                            'firstname': row.get_value('firstname'), 
-                            'lastname': row.get_value('lastname'), 
-                            'fullname': row.get_value('fullname'), 
-                            'email': row.get_value('email') 
-                        }
-                        retval += '\t\tFirst Name: %(firstname)s, \
-                                    Last Name: %(lastname)s, \
-                                    Full Name %(fullname)s, \
-                                    Email: %(email)s\n' % template_values
-                        retval += '\t\t%s\n\n\n\n' % template.render(
-                                            self.email_path, template_values)
-                        mail.send_mail(gdocs_settings['email_as'], 
-                                        template_values['email'], 
-                                        'Welcome to E-Democracy', 
-                                        template.render(self.email_path, 
-                                                        template_values))
-
-                        # Save a reference to the email we just sent for later
-                        ref = EmailReference(address = template_values['email'],
-                                            spreadsheet = spreadsheet_id,
-                                            worksheet = worksheet_id)
-                        ref.put()
-
-                        logging.debug('Emailed: %s' % template_values['email'])
+            batch_log = sendVerificationEmails(batch, persons, optout_tokens, 
+                                                log=batch_log)
+            batch_logs.append(batch_log)
 
 
+        # Process the batch_logs
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(retval)
+
 
 from gdata.spreadsheets.client import WorksheetQuery, ListQuery, CellQuery
 from gdata.spreadsheets.data import ListEntry, WorksheetEntry,\
