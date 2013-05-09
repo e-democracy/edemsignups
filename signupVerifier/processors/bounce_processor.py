aadc3ea108ffa9bb6eb93a728d9864df740a4419
Bill Bushey <wbushey@gmail.com>
2013-05-09 18:58:03 -0500

    Wrote bounce_processor functions

--- signupVerifier/processors/bounce_processor.py
+++ signupVerifier/processors/bounce_processor.py
@@ -1,25 +1,46 @@
 # coding=utf-8
 
-def createBounce(person, bounce_datetime):
+from models import Bounce
+import datetime as dt
+
+def createBounce(person, message, bounce_datetime=None):
     """
     Creates a record indicating the an email sent to the provided Person
     bounced. bounce_datetime will be recorded as the time the bounce was
     received.
 
-    Input:  person - the Person model who is opting out
+    Input:  person - the Person instance or ID of the Person instnace who is 
+                     bouncing
+            message - a string representing the bounce message received.
             bounce_datetime - datetime that the bounce was received
     Output: a Bounce model created by the logging of the Bounce.
     Side Effect: an entry is saved to the database for the opt-out
     """
-    pass
+    person = Person.verifyOrGet(person)
+    batch = person.source_batch
+    if bounce_datetime:
+        if not isinstance(bounce_datetime, dt.datetime):
+            raise TypeError("bounce_datetime must be a datetime instance")
+        bounce = Bounce(person=person,
+                        batch=batch,
+                        message=message,
+                        occurred=bounce_datetime)
+        
+    else:
+        bounce = Bounce(person=person, batch=batch, message=message)
+    bounce.put()
+    return bounce
 
 def getBounces(since=None):
     """
-    Retrieves a list of all Bounces that have occurred. If since is 
+    Retrieves an interable of all Bounces that have occurred. If since is 
     specified, will only retrieve Bounces since the specified datetime.
 
     Input:  since - optional datetime indicating that only Bounces newer
             than since should be retrieved.
-    Output: a list of Bounce instances
+    Output: an interable of Bounce instances
     """
-    pass
+    q = Bounce.all()
+    if since:
+        q.filter('occurred >=', since)
+    return q.run()
