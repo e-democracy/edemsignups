00f88e9c4986d0495bddb6143666a0edf44e3354
Bill Bushey <wbushey@gmail.com>
2013-05-09 19:20:08 -0500

    Added createBounceFromEmailAddress function

--- signupVerifier/processors/bounce_processor.py
+++ signupVerifier/processors/bounce_processor.py
@@ -1,13 +1,14 @@
 # coding=utf-8
 
-from models import Bounce
+from models import Bounce, Person
 import datetime as dt
 
 def createBounce(person, message, bounce_datetime=None):
     """
     Creates a record indicating the an email sent to the provided Person
-    bounced. bounce_datetime will be recorded as the time the bounce was
-    received.
+    bounced. If provided, bounce_datetime will be recorded as the time the 
+    bounce was received. Otherwise, the datetime of writing to the DB will be
+    recorded as the bounce time.
 
     Input:  person - the Person instance or ID of the Person instnace who is 
                      bouncing
@@ -30,6 +31,30 @@ def createBounce(person, message, bounce_datetime=None):
         bounce = Bounce(person=person, batch=batch, message=message)
     bounce.put()
     return bounce
+
+def createBounceFromEmailAddress(address, message, bounce_datetime=None):
+    """
+    Creates a record indicating that the email sent to the provided address
+    bounced. The provided address will be used to lookup the corresponding
+    Person instance. If provided, bounce_datetime will be recorded as the time 
+    the  bounce was received. Otherwise, the datetime of writing to the DB will 
+    be recorded as the bounce time.
+
+    Input:  address - the email address that caused a bounce. This will be used
+                        to find an associated Person instance.
+            message - a string representing the bounce message received.
+            bounce_datetime - datetime that the bounce was received
+    Output: a Bounce model created by the logging of the Bounce.
+    Side Effect: an entry is saved to the database for the opt-out
+    Throws: LookupError if a Person can not be found based on the provided
+            address.
+    """
+    q = Person.all()
+    q.filter('email =', address)
+    person = q.get()
+    if not person:
+        raise LookupError('No person with address %s found.' % address)
+    return createBounce(person, message, bounce_datetime)
 
 def getBounces(since=None):
     """
