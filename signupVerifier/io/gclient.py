94dba50fbcfc494e9ff92680a4c6bb90231efae8
William Bushey <wbushey@gmail.com>
2013-05-28 20:52:33 -0500

    setPermissions skips trying to delete the owner of a resource

--- signupVerifier/io/gclient.py
+++ signupVerifier/io/gclient.py
@@ -687,7 +687,8 @@ class GClient(object):
         """
         acl_feed = self.docsClient.GetResourceAcl(resource).entry
         for acl in acl_feed:
-            if acl.scope.value == settings['app_username']:
+            if acl.role.value = 'owner' or \
+                    acl.scope.value == settings['app_username']:
 		        continue
             self.docsClient.DeleteAclEntry(acl)
 
